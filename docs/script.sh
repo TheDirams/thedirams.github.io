ed36fd8b07f9130991af68bc586dc72397ba4aae
TheDirams <thedirams@gmail.com>
2021-09-03 00:04:26 +0100

    adding a seperate fonts.css

---
+++ docs/script.sh
@@ -0,0 +1,3 @@
+sed -i '/<link href="style.css" rel="stylesheet">/a <link href="fonts.css" rel="stylesheet">' setup.html
+sed -i '/<link href="style.css" rel="stylesheet">/a <link href="fonts.css" rel="stylesheet">' videos.html
+sed -i '/<link href="style.css" rel="stylesheet">/a <link href="fonts.css" rel="stylesheet">' projects.html
