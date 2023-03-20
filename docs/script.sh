19a06a8c8f68f079a1d23103efa033c7adab543e
TheDirams <thedirams@gmail.com>
2023-03-20 23:09:43 +0000

    updating website

--- docs/script.sh
+++
@@ -1,3 +0,0 @@
-sed -i '/<link href="style.css" rel="stylesheet">/a <link href="fonts.css" rel="stylesheet">' setup.html
-sed -i '/<link href="style.css" rel="stylesheet">/a <link href="fonts.css" rel="stylesheet">' videos.html
-sed -i '/<link href="style.css" rel="stylesheet">/a <link href="fonts.css" rel="stylesheet">' projects.html
