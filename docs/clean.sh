b1f6c24788597e9f247791aa535950772d6e4cfe
TheDirams <thedirams@gmail.com>
2023-03-20 23:59:03 +0000

    updating the links

---
+++ docs/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+file="videos.html"
+
+sed -i 's@index.html@https://thedirams.github.io/@g' $file
+sed -i 's@contact.html@https://thedirams.github.io/contact@g' $file
+sed -i 's@projects.html@https://thedirams.github.io/projects@g' $file
+sed -i 's@setup.html@https://thedirams.github.io/setup@g' $file
+sed -i 's@videos.html@https://thedirams.github.io/videos@g' $file
+
+
